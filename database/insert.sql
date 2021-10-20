a72f0a8f2ba72d08d82b80464a323e516f3a63ba
bibamba <maxime-tizie@hotmail.fr>
2021-10-20 10:23:02 -0500

    added new files and folers

---
+++ database/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'London', 415);
+INSERT INTO flights (origin, destination, duration) VALUES ('Shanghai', 'Paris', 760);
+INSERT INTO flights (origin, destination, duration) VALUES ('Istanbul', 'Tokyo', 700);
+INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'Paris', 435);
+INSERT INTO flights (origin, destination, duration) VALUES ('Moscow', 'Paris', 245);
+INSERT INTO flights (origin, destination, duration) VALUES ('Lima', 'New York', 455);
+
+
+locations
+INSERT INTO locations (code, name) VALUES ('JFK', 'New York');
+INSERT INTO locations (code, name) VALUES ('PVG', 'Shanghai');
+INSERT INTO locations (code, name) VALUES ('IST', 'Istanbul');
+INSERT INTO locations (code, name) VALUES ('LHR', 'London');
+INSERT INTO locations (code, name) VALUES ('SVO', 'Moscow');
+INSERT INTO locations (code, name) VALUES ('LIM', 'Lima');
+INSERT INTO locations (code, name) VALUES ('CDG', 'Paris');
+INSERT INTO locations (code, name) VALUES ('NRT', 'Tokyo');
